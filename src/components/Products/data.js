857a3f459bd6d5df33f6ac8f8a6de8b6299c848d
matespinosa <matespinosa09@gmail.com>
2020-12-10 16:20:25 -0500

    terminando el ejemplo

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -2,6 +2,10 @@ import product1 from '../../images/product-1.jpg'
 import product2 from '../../images/product-2.jpg'
 import product3 from '../../images/product-3.jpg'
 
+import sweet1 from '../../images/sweet3.jpg'
+import sweet2 from '../../images/sweet-2.jpg'
+import sweet3 from '../../images/sweet-3.jpg'
+
 export const productData = [
   {
     img: product1,
@@ -25,6 +29,33 @@ export const productData = [
     name: 'Veggie Overload',
     desc: 'Marianara sauce, basil italian sausage, roma tomatoes, olives, and pesto',
     price: 'COP$ 15.000',
+    button: 'Add to Cart'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: sweet1,
+    alt: 'Donuts',
+    name: 'Doughlicious',
+    desc: 'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+    price: 'COP$ 8.000',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet2,
+    alt: 'Ice Cream',
+    name: 'Caramel Wonder',
+    desc: 'Vanilla ice creame covered width caramel and chocolate glaze topped with a coco stick',
+    price: 'COP$ 12.000',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet3,
+    alt: 'Brownie',
+    name: 'Brownie Brunch',
+    desc: 'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: 'COP$ 10.000',
     button: 'Add to Cart'
   }
 ]
